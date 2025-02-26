0aceb76fb5243ad06e0100a61bdbb67efc7e4626
Afonso_Marques <afonsolobo945@gmail.com>
2025-02-26 09:59:29 +0000

    coisas coisadas

---
+++ WebAlunoMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAlunoMVC.MvcApplication" Language="C#" %>
---
+++ WebAlunoMVC/Models/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAlunoMVC.Models
+{
+    public class Aluno
+    {
+        [Required]
+        [Display(Name = "Numero de Aluno")]
+        public int NAluno { get; set; }
+
+        [Required]
+        [Display(Name = "Primeiro Nome")]
+        public int PrimeiroNome { get; set; }
+
+        [Required]
+        [Display(Name = "Último Nome")]
+        public int UltimoNome { get; set; }
+
+        [Required]
+        public int Morada { get; set; }
+
+        [Required]
+        public Sexo Sexo { get; set; }
+
+        [Required]
+        [Display(Name = "Data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        [Range(1, 12)]
+        [Display(Name = "Ano de escolaridade")]
+        public int AnoEscolaridade { get; set; }
+
+        [Display(Name = "Imagem")]
+        public string ImagePath { get; set; }
+        public HttpPostedFileBase Imagem { get; set; }
+    }
+}
